c32db67c2ea2712be31f28a08d37b0049c1351e1
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-08-07 15:51:10 +1200

    referee

--- verification/referee.py
+++ verification/referee.py
@@ -34,14 +34,19 @@ from checkio.referees import checkers
 
 from tests import TESTS
 
+cover = """def cover(func, data):
+    return func(tuple(data))
+"""
+
 api.add_listener(
     ON_CONNECT,
     CheckiOReferee(
         tests=TESTS,
         cover_code={
-            'python-27': cover_codes.unwrap_args,  # or None
-            'python-3': cover_codes.unwrap_args
+            'python-27': cover,  # or None
+            'python-3': cover
         },
+        function_name="left_join",
         # checker=None,  # checkers.float.comparison(2)
         # add_allowed_modules=[],
         # add_close_builtins=[],
