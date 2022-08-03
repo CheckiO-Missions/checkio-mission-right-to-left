897f32bbededde1de9632f987f2d0f775370d62e
Alexander Blaschuk <blaschuk95@gmail.com>
2022-08-03 16:27:01 +0300

    next-api uk-hints

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -1,17 +1,9 @@
 //Dont change it
 //Dont change it
-requirejs(['ext_editor_io', 'jquery_190'],
-    function (extIO, $) {
-        
-        var $tryit;
 
-        var io = new extIO({
-            multipleArguments: false,
-            functions: {
-                python: 'left_join',
-                js: 'leftJoin'
-            }
-        });
+requirejs(['ext_editor_io2', 'jquery_190'],
+    function (extIO, $) {
+        var io = new extIO({});
         io.start();
     }
 );
